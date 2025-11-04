8ce0fdca2a7ca4c451f537a36dab7b90db329e8c
Shane Donaghy <donaghys@tcd.ie>
2025-11-04 19:18:00 +0000

    second image component

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,14 @@ export default function Page() {
                         width={1000}
                         height={760}
                         className="hidden md:block"
-                        alt="Screenshots of the dashboar project showing desktop version"
+                        alt="Screenshots of the dashboard project showing desktop version"
+                    />
+                    <Image
+                        src="/hero-mobile.png"
+                        width={560}
+                        height={620}
+                        className="block md:hidden"
+                        alt="Screen shots of the dashboard project mobile version"
                     />
                 </div>
                 <div
